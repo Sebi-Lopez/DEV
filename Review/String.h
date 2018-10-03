12357f249ffa91a0f7b90cf578055e1dc0172535
Sebi-Lopez <sebi8818@gmail.com>
2018-10-03 14:01:44 +0200

    String not fully implemented + Orthogonal

--- Review/String.h
+++ Review/String.h
@@ -1,27 +1,68 @@
 #ifndef STRING_H
 #define STRING_H
 #include <string.h>
+#include <assert.h>
 class String {
 
 private: 
-	char* ch;
-	int size; 
+	char* string = nullptr;
+	unsigned int allocated_memory = 0u; 
 
 
 public: 
 
-	String() {
-		size = 0; 
-	}
-	String(char* string){}
-
-	void MakeString(int size, char* string) {
-		for (int i = 0; i < size; ++i)
+	String() {}
+	String(const char* string)
+	{
+		assert(string != nullptr);
+		if (string != nullptr) 
 		{
-			ch[i] = string[i];
+			allocated_memory = strlen(string) + 1;
+			this->string = new char[allocated_memory];
+			strcpy_s(this->string, allocated_memory, string);
 		}
 	}
 
+	String(const String& chain)
+	{
+		assert(chain.string != nullptr);
+		if (chain.string != nullptr)
+		{
+			allocated_memory = chain.allocated_memory;
+			this->string = new char[allocated_memory];
+			strcpy_s(this->string, allocated_memory, chain.string); 
+		}
+	}
+
+	String operator=(const char* chain)
+	{
+		// Si el tamany de chain es mes petit o igual al que tenim no cal fer un new. 
+		if (strlen(chain) + 1 <= this->allocated_memory)
+		{
+			allocated_memory = strlen(chain) + 1; 
+			strcpy_s(string, allocated_memory, chain);
+		}
+	}
+
+	String operator=(const String& chain)
+	{
+		return String(chain.string);
+	}
+	
+	void GetChar() 
+	{
+		
+	}
+
+	~String() 
+	{
+		if (string != nullptr)
+		{
+			delete[] string;
+			string = nullptr; 
+			allocated_memory = 0u;
+		}
+	}
 };
 
 
--- Review/main.cpp
+++ Review/main.cpp
@@ -1,20 +1,13 @@
 #include <stdio.h>
 #include <iostream>
 #include "Vec3.h"
-
+#include "String.h"
 
 
 int main() {
 	
-	vec3 <int>vec(50, 30, 450);
-	vec3 <int>vec2(50, 30, 500);
-	int distance; 
-
-	vec3 <float>vecf(200.5, 300.5, 400.5);
-	vecf.normalize(); 
-
-	distance = vec.distance_to(vec2);
-
+	String a("hello");
+	
 	system("pause");
 	return 0; 
 }
