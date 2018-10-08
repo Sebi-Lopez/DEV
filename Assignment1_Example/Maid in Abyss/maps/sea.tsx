dbbf15da591f71508c5664a621fea7262e9ea244
Sebi Lopez <sebi8818@gmail.com>
2018-10-08 18:54:45 +0200

    Added Example

---
+++ Assignment1_Example/Maid in Abyss/maps/sea.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="sea" tilewidth="16" tileheight="16" tilecount="42" columns="7">
+ <image source="../../../../Desktop/CITM/DEV/6e5cdcabcc47894f1b886a41a360a7ff92a230e1/GDM-Magic-Cliffs-Environment/PNG/sea.png" width="112" height="96"/>
+</tileset>
